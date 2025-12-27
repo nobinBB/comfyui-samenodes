433750ec57c5d3826716fbf9196e442411ec8548
nobinBB <nobinlog99@gmail.com>
2025-12-27 12:17:16 +0900

    fix

--- __init__.py
+++ __init__.py
@@ -1,22 +1,23 @@
-"""
-ComfyUI Same Nodes - Custom Nodes Package
-Float to String conversion utilities, Batch Image Processor, LoRA Wildcard Generator, Civitai Bulk Downloader, Extract Prompt from Image, and Queue Empty Checker
-"""
-
 from .float_to_string import NODE_CLASS_MAPPINGS as FLOAT_MAPPINGS
 from .float_to_string import NODE_DISPLAY_NAME_MAPPINGS as FLOAT_DISPLAY_MAPPINGS
+
 from .batch_processor import NODE_CLASS_MAPPINGS as BATCH_MAPPINGS
 from .batch_processor import NODE_DISPLAY_NAME_MAPPINGS as BATCH_DISPLAY_MAPPINGS
+
 from .lora_wildcard_generator import NODE_CLASS_MAPPINGS as LORA_MAPPINGS
 from .lora_wildcard_generator import NODE_DISPLAY_NAME_MAPPINGS as LORA_DISPLAY_MAPPINGS
+
 from .civitai_bulk_downloader import NODE_CLASS_MAPPINGS as CIVITAI_MAPPINGS
 from .civitai_bulk_downloader import NODE_DISPLAY_NAME_MAPPINGS as CIVITAI_DISPLAY_MAPPINGS
+
 from .extract_prompt_from_image import NODE_CLASS_MAPPINGS as EXTRACT_MAPPINGS
 from .extract_prompt_from_image import NODE_DISPLAY_NAME_MAPPINGS as EXTRACT_DISPLAY_MAPPINGS
+
 from .repeat_text_lines import NODE_CLASS_MAPPINGS as REPEAT_MAPPINGS
 from .repeat_text_lines import NODE_DISPLAY_NAME_MAPPINGS as REPEAT_DISPLAY_MAPPINGS
-from .queue_empty_checker import NODE_CLASS_MAPPINGS as QUEUE_MAPPINGS
-from .queue_empty_checker import NODE_DISPLAY_NAME_MAPPINGS as QUEUE_DISPLAY_MAPPINGS
+
+
+
 from .prompt_extractor_posneg import NODE_CLASS_MAPPINGS as POSNEG_MAPPINGS
 from .prompt_extractor_posneg import NODE_DISPLAY_NAME_MAPPINGS as POSNEG_DISPLAY_MAPPINGS
 
@@ -30,8 +31,8 @@ NODE_CLASS_MAPPINGS = {
     **CIVITAI_MAPPINGS,
     **EXTRACT_MAPPINGS,
     **REPEAT_MAPPINGS,
-    **QUEUE_MAPPINGS,
     **POSNEG_MAPPINGS,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -41,10 +42,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     **CIVITAI_DISPLAY_MAPPINGS,
     **EXTRACT_DISPLAY_MAPPINGS,
     **REPEAT_DISPLAY_MAPPINGS,
-    **QUEUE_DISPLAY_MAPPINGS,
     **POSNEG_DISPLAY_MAPPINGS,
 }
 
-
-# Export the mappings for ComfyUI to discover
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
